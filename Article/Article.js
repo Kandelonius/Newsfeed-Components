c4545bd83a5fc05a38bcea0f2e4f238d95510cc4
Shane Kaestner <shane.kaestner@yahoo.com>
2020-04-08 18:17:58 -0700

    I was working on the wrong section this whole time... Did a lot of work in the Article.js section creating my article div.

--- Article/Article.js
+++ Article/Article.js
@@ -98,17 +98,41 @@ const data = [
 
     <span class='expandButton'></span>
   </div>
-
   Hint: You will need to use createElement more than once here!
 
   Your function should take either an object as it's one argument, or 5 separate arguments mapping to each piece of the data object above.
-
-  Step 2: Add an event listener to the expandButton span. This event listener should toggle the class 'article-open' on the 'article' div.
-
-  Step 3: return the entire component.
-
-  Step 4: Map over the data, creating a component for each oject and add each component to the DOM as children of the 'articles' div.
-
-  Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
-
 */
+function createComponent({ title, date, paragraphOne, paragraphTwo, paragraphThree}){
+  const article = document.createElement('div');
+  const articleTitle = document.createElement('h2');
+  const articleDate = document.createElement('p');
+  const articleParagraphOne = document.createElement('p');
+  const articleParagraphTwo = document.createElement('p');
+  const articleParagraphThree = document.createElement('p');
+  const expandButton = document.createElement('span');
+  article.appendChild(articleTitle);
+  article.appendChild(articleDate);
+  article.appendChild(articleParagraphOne);
+  article.appendChild(articleParagraphTwo);
+  article.appendChild(articleParagraphThree);
+  article.appendChild(expandButton);
+  article.classList.add('article');
+  articleDate.classList.add('date');
+  expandButton.classList.add('expandButton');
+  articleTitle.textContent = title;
+  articleDate.textContent = date;
+  articleParagraph1 = paragraphOne;
+  articleParagraph2 = paragraphTwo;
+  articleParagraph3 = paragraphThree;
+  // Step 2: Add an event listener to the expandButton span. This event listener should toggle the class 'article-open' on the 'article' div.
+  // console.log(article);
+  const articleToggle = (event) => {
+    article.classList.toggle('article-open');
+  };
+  expandButton.addEventListener('click', articleToggle);
+  // Step 3: return the entire component.
+};
+  // Step 4: Map over the data, creating a component for each oject and add each component to the DOM as children of the 'articles' div.
+
+  // Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
+// createComponent({});
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -8,7 +8,11 @@ let menuItems = [
   'Music',
   'Log Out'
 ];
-// const get = (selector) => {document.querySelector(selector)}
+const get = (selector) => {
+  return document.querySelector(selector)
+}
+
+// debugger
 /* 
   Step 1: Write a function that will create a menu component as seen below:
 
@@ -22,27 +26,40 @@ let menuItems = [
 function makeMenuComponent(listArray){
   const menuDiv = document.createElement('div');
   const ulEl = document.createElement('ul');
-  
   menuDiv.appendChild(ulEl);
-  
+  menuDiv.classList.add('menu');
   // ulEl.appendChild(listArray);
 //  Step 2: Inside this function, iterate over the array creating a list item <li> element for each item in the array. Add those items to the <ul>
   // console.log(menuDiv);
   // debugger
-  const listElements = menuItems.forEach(menuItems =>{
+  const listElements = listArray.forEach(listArray =>{
     const listItem = document.createElement('li');
-    listItem.textContent = menuItems;
+    listItem.textContent = listArray;
     ulEl.appendChild(listItem);
   })
-  console.log(menuDiv);
-}
 
-// makeMenuComponent([]);
-  //Step 3: Using a DOM selector, select the menu button (the element with a class of 'menu-button') currently on the DOM.
-const menuButton = get('.menu-button');
-  //Step 4: add a click event listener to the menu button. When clicked it should toggle the class 'menu--open' on the menu (your div with a 'menu' class).
-
+  // console.log(menuDiv);
+    //Step 3: Using a DOM selector, select the menu button (the element with a class of 'menu-button') currently on the DOM.
+    const panelElements = panelData.map(panelData => makePanel(panelData))
+  const menuButton = get('.menu-button');
+  // const menuButton = document.querySelector('.menu-button');
+  // debugger
+    //Step 4: add a click event listener to the menu button. When clicked it should toggle the class 'menu--open' on the menu (your div with a 'menu' class).
+  //   console.log(menuButton);
+  menuButton.addEventListener('click', event =>{
+    menuDiv.classList.toggle('menu--open');
   //Step 5: return the menu component.
-
+    
+  })
+  return menuDiv;
+}
   //Step 6: add the menu component to the DOM.
-
+/*
+const articleContainer = document.querySelector('.articles');
+const articleElements = menuItems.map(menuItems =>{
+    makeMenuComponent(menuItems)}) 
+  // console.log(articleContainer);
+  articleContainer.forEach(articleContainer => {
+    articleContainer.appendChild();
+  });
+*/
